58a103005f3fd38502ee93e4a6136dd77391f8c8
Ricardo Ruiz <rgomez051999@gmail.com>
2022-11-14 01:54:53 -0600

    small changes were made to response_helper for good coding practice purposes

--- back-end/helpers/response_helper.go
+++ back-end/helpers/response_helper.go
@@ -17,6 +17,7 @@ func JSONResponse(w http.ResponseWriter, code int, payload interface{}) error {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
+
 	_, err = w.Write(response)
 	if err != nil {
 		fmt.Println(err)
